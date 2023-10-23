6902d90a24534d29fd9ea9e7b07006af4e454cea
Daniel Perez <dapehe94@gmail.com>
2023-10-24 00:07:15 +0100

    update readme

--- src/SubscribeServiceProvider.php
+++ src/SubscribeServiceProvider.php
@@ -8,6 +8,7 @@ class SubscribeServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+
         $this->publishes([
             \dirname(__DIR__).'/config/subscribe.php' => config_path('subscribe.php'),
         ], 'config');
@@ -15,13 +16,16 @@ class SubscribeServiceProvider extends ServiceProvider
         $this->publishes([
             \dirname(__DIR__).'/migrations/' => database_path('migrations'),
         ], 'migrations');
+
     }
 
     public function register()
     {
+
         $this->mergeConfigFrom(
             \dirname(__DIR__).'/config/subscribe.php',
             'subscribe'
         );
+        
     }
 }
